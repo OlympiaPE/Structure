1fff567ac3623ba850c8bf832e32d8454327a8dc
RemBog <87894259+RemBog@users.noreply.github.com>
2024-06-02 23:38:56 +0200

    Create CommandManager.php

---
+++ src/ExampleName/managers/types/CommandManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Olympia\Kitpvp\managers\types;
+
+use ExampleName\Loader;
+use ExampleName\managers\Manager;
+use ExampleName\utils\FileUtil;
+use pocketmine\command\Command;
+use Symfony\Component\Filesystem\Path;
+
+class CommandManager extends Manager
+{
+    /**
+     * @return void
+     */
+    public function onLoad(): void
+    {
+        FileUtil::callDirectory(Path::join("commands"), function(string $name): void {
+            $command = new $name();
+            if ($command instanceof Command) {
+                Loader::getInstance()->getServer()->getCommandMap()->register($command->getName(), $command);
+            }
+        });
+    }
+}
