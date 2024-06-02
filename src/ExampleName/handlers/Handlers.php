869df58398d5547ff8e07888a4d76385cff7bee1
RemBog <87894259+RemBog@users.noreply.github.com>
2024-06-02 23:41:24 +0200

    Update Handlers.php
    Add register handler log and auto register handler

--- src/ExampleName/handlers/Handlers.php
+++ src/ExampleName/handlers/Handlers.php
@@ -2,7 +2,9 @@
 
 namespace ExampleName\handlers;
 
+use ExampleName\Loader;
 use pocketmine\utils\RegistryTrait;
+use ReflectionClass;
 
 /**
  //* @method static AntiVPN ANTI_VPN() example
@@ -21,6 +23,7 @@ class Handlers
     protected static function register(string $name, Handler $handler) : void{
         $handler->onLoad();
         self::_registryRegister($name, $handler);
+        Loader::getInstance()->getLogger()->info("§eThe $name handler has been successfully registered");
     }
 
     /**
@@ -43,5 +46,24 @@ class Handlers
      */
     protected static function setup(): void
     {
+        $reflectionClass = new ReflectionClass(self::class);
+        $namespace = $reflectionClass->getNamespaceName();
+        $docComment = $reflectionClass->getDocComment();
+
+        $matches = [];
+        preg_match_all('/@method\s+static\s+(\S+)\s+([^\s()]+)\(\)/', $docComment, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+
+            $className = $match[1];
+            $handlerClass = "\\$namespace\\types\\$className";
+            $handler = new $handlerClass();
+
+            if ($handler instanceof Handler) {
+                self::register(strtolower($match[2]), $handler);
+            }else{
+                Loader::getInstance()->getLogger()->error("[Handler] The $className class does not inherit from Handler !");
+            }
+        }
     }
 }
