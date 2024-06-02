9a7d82e41792b6dcf93d1cac2448e82d0e860a4b
RemBog <87894259+RemBog@users.noreply.github.com>
2024-06-02 23:35:41 +0200

    Update Managers.php
    Add auto register, register messsage and save on disable

--- src/ExampleName/managers/Managers.php
+++ src/ExampleName/managers/Managers.php
@@ -3,6 +3,7 @@
 namespace ExampleName\managers;
 
 use ExampleName\managers\types\ListenerManager;
+use ExampleName\Loader;
 use pocketmine\utils\RegistryTrait;
 
 /**
@@ -22,6 +23,7 @@ class Managers
     protected static function register(string $name, Manager $manager) : void{
         $manager->onLoad();
         self::_registryRegister($name, $manager);
+        Loader::getInstance()->getLogger()->info("§aThe $name manager has been successfully registered");
     }
 
     /**
@@ -44,6 +46,36 @@ class Managers
      */
     protected static function setup(): void
     {
-        self::register("listener", new ListenerManager());
+        $reflectionClass = new ReflectionClass(self::class);
+        $namespace = $reflectionClass->getNamespaceName();
+        $docComment = $reflectionClass->getDocComment();
+
+        $matches = [];
+        preg_match_all('/@method\s+static\s+(\S+)\s+([^\s()]+)\(\)/', $docComment, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+
+            $className = $match[1];
+            $managerClass = "\\$namespace\\types\\$className";
+            $manager = new $managerClass();
+
+            if ($manager instanceof Manager) {
+                self::register(strtolower($match[2]), $manager);
+            }else{
+                Loader::getInstance()->getLogger()->error("[Manager] The $className class does not inherit from Manager !");
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public static function save(): void
+    {
+        foreach (self::getAll() as $manager) {
+            if ($manager instanceof Manager && $manager->isRequireSaveOnDisable()) {
+                $manager->save();
+            }
+        }
     }
 }
