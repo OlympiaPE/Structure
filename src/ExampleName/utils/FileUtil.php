ef3e0c70d6d2e2875bc852b4566c329211da5fc0
RemBog <87894259+RemBog@users.noreply.github.com>
2024-06-02 23:43:12 +0200

    Add subdirectory support

--- src/ExampleName/utils/FileUtil.php
+++ src/ExampleName/utils/FileUtil.php
@@ -11,21 +11,23 @@ class FileUtil
      * @param string $directory
      * @param callable $callable
      * @param string|null $filesType
+     * @param array $ignoreNamespace
      * @return void
      */
-    public static function callDirectory(string $directory, callable $callable, string $filesType = null): void
+    public static function callDirectory(string $directory, callable $callable, string $filesType = null, array $ignoreNamespace = []): void
     {
         $main = explode("\\", Loader::getInstance()->getDescription()->getMain());
         unset($main[array_key_last($main)]);
         $main = implode("/", $main);
         $directory = rtrim(str_replace(DIRECTORY_SEPARATOR, "/", $directory), "/");
-        $dir = Loader::getInstance()->getFile() . "src/{$main}/" . $directory;
+        $dir = Loader::getInstance()->getFile() . "src/$main/" . $directory;
 
         foreach (PathScanner::scanDirectory($dir, $filesType ?? ["php"]) as $file) {
-            $namespaceDirectory = str_replace("/", "\\", $directory);
-            $namespaceMain = str_replace("/", "\\", $main);
-            $namespace = $namespaceMain . "\\$namespaceDirectory\\" . basename($file, ".php");
-            $callable($namespace);
+
+            $namespace = str_replace(["/", ".php"], ["\\", ""], strstr($file, $main));
+            if(!in_array($namespace, $ignoreNamespace)) {
+                $callable($namespace);
+            }
         }
     }
 }
